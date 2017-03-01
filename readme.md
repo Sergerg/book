f9228328e8386279d8c235e10646fd15fe4f9eb5
galichanin <galichanin@elewise.com>
2017-03-02 00:11:23 +0400

    Run with tomcat. Simple Servlet.

--- readme.md
+++ readme.md
@@ -8,4 +8,5 @@ TODO:
 2. Create 3 Book, 2 Reader, 1 Reader read 0 Book, 1 Reader read 2 Book. Test 1 query. 
 3. Run tomcat.
 4. Create servlet.
-5. REST.
+5. War.
+6. REST.
--- src/main/java/org/serger/BookApplication.java
+++ src/main/java/org/serger/BookApplication.java
@@ -7,19 +7,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 @SpringBootApplication
 public class BookApplication {
 
     private static final Logger log = LoggerFactory.getLogger(BookApplication.class);
 
+
+	@SuppressWarnings("serial")
+	@Bean
+	public Servlet dispatcherServlet() {
+		return new GenericServlet() {
+			@Override
+			public void service(ServletRequest req, ServletResponse res)
+					throws ServletException, IOException {
+				res.setContentType("text/plain");
+				res.getWriter().append("Hello World");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
 	}
 
-	@Bean
-    CommandLineRunner runner() {
-	    return args -> {
-	        log.debug("runner...");
-        };
-    }
 }
