12d370cd3dd23e8d8dabbe820716f62efcccd77e
galichanin <galichanin@elewise.com>
2017-03-01 20:45:08 +0400

    Spring properties.

--- src/main/java/org/serger/BookApplication.java
+++ src/main/java/org/serger/BookApplication.java
@@ -1,12 +1,25 @@
 package org.serger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(BookApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
 	}
+
+	@Bean
+    CommandLineRunner runner() {
+	    return args -> {
+            log.debug("runner...");
+        };
+    }
 }
