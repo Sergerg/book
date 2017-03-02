038b37ddc2d496ec06e4d2fc70148233eeff4920
galichanin <galichanin@elewise.com>
2017-03-02 22:50:05 +0400

    Servlet inject test + log.

--- src/main/java/org/serger/servlets/BooksServlet.java
+++ src/main/java/org/serger/servlets/BooksServlet.java
@@ -1,5 +1,10 @@
 package org.serger.servlets;
 
+import org.serger.services.mapper.BookMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -11,11 +16,19 @@ import java.io.IOException;
  * Created by galichanin on 02.03.2017.
  */
 public class BooksServlet extends GenericServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(BooksServlet.class);
+
+    @Autowired
+    BookMapper bookMapper;
+
     @Override
     public void service(ServletRequest req, ServletResponse res)
             throws ServletException, IOException {
+        log("Book servlet.");
+
         res.setContentType("text/plain");
-        res.getWriter().append("Hello World");
+        res.getWriter().append("Hello World="+bookMapper.selectAll().size());
     }
 
 }
