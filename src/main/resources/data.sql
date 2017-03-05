876fefa6acc4cca36a4995d6b26114d89b34afa3
galichanin <galichanin@elewise.com>
2017-03-05 18:03:58 +0400

    Data init sql.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into book (title, author) values ('Book 1','Author 1');
+insert into book (title, author) values ('Book 2','Author 2');
+insert into book (title, author) values ('Book 3','Author 2');
+insert into book (title, author) values ('Book 4','Author 2');
+insert into book (title, author) values ('Book 5','Author 3');
+insert into book (title, author) values ('Book 6','Author 4');
+insert into book (title, author) values ('Book 7','Author 4');
+
+insert into reader (name) values ('Reader 1');
+insert into reader (name) values ('Reader 2');
+insert into reader (name) values ('Reader 3');
+insert into reader (name) values ('Reader 4');
+insert into reader (name) values ('Reader 5');
+insert into reader (name) values ('Reader 6');
+insert into reader (name) values ('Reader 7');
+insert into reader (name) values ('Reader 8');
