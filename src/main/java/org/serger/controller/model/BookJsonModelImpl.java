f54cde62e0908a94f4cd21a02e2e9c85173bf8f7
galichanin <galichanin@elewise.com>
2017-03-06 09:36:40 +0400

    Stream.

--- src/main/java/org/serger/controller/model/BookJsonModelImpl.java
+++ src/main/java/org/serger/controller/model/BookJsonModelImpl.java
@@ -2,6 +2,7 @@ package org.serger.controller.model;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.serger.domain.entity.Book;
 import org.serger.domain.entity.BookReader;
 import org.serger.domain.mapper.BookMapper;
 import org.serger.domain.mapper.BookReaderMapper;
@@ -9,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
 
 /**
  * Created by galichanin on 03.03.2017.
@@ -35,17 +38,7 @@ public class BookJsonModelImpl implements BookJsonModel {
 
     @Override
     public String selectByReader(long readerId) {
-        JSONArray jsonBooks = new JSONArray();
-        bookReaderMapper.selectBooksByReader(readerId).stream().forEach( book1 -> {
-            JSONObject jsonObjectBook = new JSONObject();
-            jsonObjectBook.put("id", book1.getId());
-            jsonObjectBook.put("title", book1.getTitle());
-            jsonObjectBook.put("author", book1.getAuthor());
-            jsonBooks.add(jsonObjectBook);
-        }); // XXX: Limit!!!
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("books", jsonBooks);
-        return jsonObject.toJSONString();
+        return selectBooks(bookReaderMapper.selectBooksByReader(readerId).stream());  // XXX: Limit!!!
     }
 
     @Override
@@ -65,17 +58,7 @@ public class BookJsonModelImpl implements BookJsonModel {
 
     @Override
     public String selectAll() {
-        JSONArray jsonBooks = new JSONArray();
-        bookMapper.selectAll().stream().forEach( book1 -> {
-            JSONObject jsonObjectBook = new JSONObject();
-            jsonObjectBook.put("id", book1.getId());
-            jsonObjectBook.put("title", book1.getTitle());
-            jsonObjectBook.put("author", book1.getAuthor());
-            jsonBooks.add(jsonObjectBook);
-        }); // XXX: Limit!!!
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("books", jsonBooks);
-        return jsonObject.toJSONString();
+        return selectBooks(bookMapper.selectAll().stream());  // XXX: Limit!!!
     }
 
     @Override
@@ -86,6 +69,25 @@ public class BookJsonModelImpl implements BookJsonModel {
     @Override
     public void insertReader(long readerId, long bookId) {
         bookReaderMapper.insert(new BookReader(null, bookId, readerId));
+    }
+
+    /**
+     * Query stream -> JSON
+     * @param stream
+     * @return
+     */
+    private String selectBooks(Stream<Book> stream) {
+        JSONArray jsonBooks = new JSONArray();
+        stream.forEach( book1 -> {
+            JSONObject jsonObjectBook = new JSONObject();
+            jsonObjectBook.put("id", book1.getId());
+            jsonObjectBook.put("title", book1.getTitle());
+            jsonObjectBook.put("author", book1.getAuthor());
+            jsonBooks.add(jsonObjectBook);
+        });
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("books", jsonBooks);
+        return jsonObject.toJSONString();
     }
 
 }
